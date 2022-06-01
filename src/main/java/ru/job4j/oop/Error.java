689658c37e8f41a2136ff551815cda564ac8cdbc
Sarabik <dalilija@inbox.lv>
2022-06-01 22:24:19 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,29 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активна: " + active);
+        System.out.println("Статус: " + status);
+        System.out.println("Сообщение: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        error1.printInfo();
+        Error error2 = new Error(true, 65, "Обнаружена ошибка!");
+        error2.printInfo();
+    }
+}
