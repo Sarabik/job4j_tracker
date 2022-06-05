7350cb0a87266ff644c87eb9107da4b7694cf41c
Sarabik <dalilija@inbox.lv>
2022-06-05 13:58:04 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,19 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passengers(int count) {
+
+    }
+
+    @Override
+    public double fill(double fuel) {
+        return 0;
+    }
+}
