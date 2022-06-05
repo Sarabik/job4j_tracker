8d0c8689d0d390adb56c01f8fb4ac7022260eeb4
Sarabik <dalilija@inbox.lv>
2022-06-05 15:49:32 +0300

    3.1. Приведение типов. Повышение типа и понижение типа. [#181322 #309236]

---
+++ src/main/java/ru/job4j/poly/BusNew.java
@@ -0,0 +1,8 @@
+package ru.job4j.poly;
+
+public class BusNew implements Vehicle {
+    @Override
+    public void move() {
+        System.out.println("Автобус едет по дороге.");
+    }
+}
---
+++ src/main/java/ru/job4j/poly/VehicleExample.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class VehicleExample {
+    public static void main(String[] args) {
+        Vehicle airplane1 = new Airplane();
+        Vehicle airplane2 = new Airplane();
+        Vehicle train1 = new Train();
+        Vehicle train2 = new Train();
+        Vehicle bus1 = new BusNew();
+        Vehicle bus2 = new BusNew();
+        Vehicle bus3 = new BusNew();
+        Vehicle[] vehicles = {train1, airplane2, bus1, bus2, airplane1, train2, bus3};
+        for (Vehicle vehicle : vehicles) {
+            vehicle.move();
+        }
+    }
+}
