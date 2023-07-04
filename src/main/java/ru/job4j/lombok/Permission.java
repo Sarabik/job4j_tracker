8c241838f38a1c089586838d7daf6b27651f3550
Sarabik <dalilija@inbox.lv>
2023-07-04 13:55:31 +0300

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/lombok/Permission.java
@@ -0,0 +1,19 @@
+package ru.job4j.lombok;
+
+import lombok.*;
+
+import java.util.List;
+
+@Builder
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Getter
+public class Permission {
+    @EqualsAndHashCode.Include
+    private int id;
+
+    private String name;
+
+    @Singular("rules")
+    private List<String> rules;
+}
