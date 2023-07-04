829d364b65cfa43e99eb49b1bfcb0543b1571ff9
Sarabik <dalilija@inbox.lv>
2023-07-04 12:06:05 +0300

    1. Lombok. Настройка IDEA [#504888 #427501]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 @Data
 public class Item {
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
 
     public Item() {
@@ -28,13 +29,5 @@ public class Item {
         this.id = id;
         this.name = name;
         this.created = created;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" + "id=" + id
-                + ", name='" + name + '\''
-                + ", created=" + created.format(FORMATTER)
-                + '}';
     }
 }
